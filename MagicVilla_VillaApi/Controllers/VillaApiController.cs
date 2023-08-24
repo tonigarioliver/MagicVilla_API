cf3bbeadc511adaac7fc5875d06665b08d471d2d
tonigarioliver <tonietmoscari@gmail.com>
2023-08-25 00:53:13 +0200

    Add controller

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -15,5 +15,18 @@ namespace MagicVilla_VillaApi.Controllers
         {
             return VillaStore.VillasList;
         }
+        [HttpGet]
+        [Route("GetVillaById/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0) { return BadRequest(); }
+            var villa=VillaStore.VillasList.FirstOrDefault(x => x.Id == id);
+            if (villa is null) { return NotFound(); }
+            return Ok(villa);   
+        }
     }
 }
