e82c095ff9be3a09b83a47206cea9a48ad1b8b09
tonigarioliver <tonietmoscari@gmail.com>
2023-08-24 21:20:21 +0200

    First Controller

---
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAllVillas")]
+        public ActionResult<IEnumerable<VillaDTO>> GetAll()
+        {
+            return VillaStore.VillasList;
+        }
+    }
+}
---
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaApi.Models.DTOs;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> VillasList=new List<VillaDTO>
+                        {
+                            new VillaDTO{Id = 1,Name = "Test1"},
+                            new VillaDTO { Id = 2, Name = "Test2"}
+                        };
+    }
+}
