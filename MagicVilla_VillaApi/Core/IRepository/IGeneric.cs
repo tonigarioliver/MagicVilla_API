59c8adb99acd39d3fff9906eb7f5ad256c782fda
tonigarioliver <tonietmoscari@gmail.com>
2023-08-28 00:19:17 +0200

    starting repository

---
+++ MagicVilla_VillaApi/Core/IRepository/IGeneric.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaApi.Core.IRepository
+{
+    public interface IGeneric<T>where T : class
+    {
+        public Task<IList> GetAllAsync(Expression<Func<T, bool>>? filter = null,bool tracked=true);
+        public Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
+        public Task<T> CreateAsync(T entity);
+        public Task<bool> Update(T entity);
+        public Task<bool> Delete(T entity);
+    }
+}
---
+++ MagicVilla_VillaApi/Core/Repository/Generic.cs
@@ -0,0 +1,70 @@
+﻿using MagicVilla_VillaApi.Core.IRepository;
+using MagicVilla_VillaApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaApi.Core.Repository
+{
+    public class Generic<T>:IGeneric<T> where T : class
+    {
+        protected AppDbContext _appDbContext;
+        protected DbSet<T> _dbSet;
+        protected ILogger _logger;
+
+        public Generic(AppDbContext appDbContext,ILogger logger)
+        {
+            _appDbContext = appDbContext;
+            _logger = logger;
+            _dbSet = _appDbContext.Set<T>();
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<bool> Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            return true;
+        }
+
+        public async Task<IList> GetAllAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query =_dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> Update(T entity)
+        {
+            _dbSet.Update(entity);
+            return true;
+        }
+    }
+}
